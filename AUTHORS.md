a6ba20f191e6389776b54df220d048d1f243b8c0
lukemenard <lukemenard@gmail.com>
2019-08-02 10:52:07 -0600

    Authors and Requirements

---
+++ AUTHORS.md
@@ -0,0 +1,15 @@
+=======
+Credits
+=======
+
+Development Leads
+----------------
+
+* Luke Menard <lukemenard@gmail.com>,  'Git <https://github.com/lukemenard>'
+* Rob Ballantyne <raballantyne@gmail.com>, 'Git <https://github.com/Jerkbert>'
+
+
+Contributors
+------------
+
+None
--- lib/models/help.rb
+++ lib/models/help.rb
@@ -9,7 +9,42 @@ class Help
 
   def self.main_menu_help
     tty_runner
-    puts "poop"
+    puts ""
+    puts ""
+    puts "
+    Step 1: Login
+        •	Create a new username if you’re new to TreeFind
+        •	If you’ve used TreeFind, you can select your username from a list of previous users
+
+    Step 2: Choose what you want to do
+    From the TreeFind main menu, you can do any of the following:
+        •	Identify a tree
+        •	See a list of all trees available in the TreeFind database
+        •	Add a tree to your personal list of favorites
+        •	See your personal list of favorite trees
+        •	Learn how to use the app
+
+    Step 3:  Identify a tree
+        1.	Answer a series of questions about your tree to narrow the list of available trees
+        2.	See all trees from the database that meet the criteria you entered
+        3.	Save any of the identified trees to your personal list of favorites
+
+    Step 4: See a list of all trees available in the TreeFind database
+        1.	Scroll through a list of all trees available in the TreeFind database
+        2.	Select a tree from the list
+        3.	See additional information and characteristics about the selected tree
+
+    Step 5: Add a tree to your personal list of favorites
+        1.	Scroll through a list of all trees available in the TreeFind database
+        2.	Select a tree from the list
+        3.	This tree is added to your personal list of favorite trees
+
+    Step 6: See your personal list of favorite trees
+        1.	Outputs a list of all the trees your saved to your favorites via Step 5
+
+    Step 7: Learn how to use the app
+        1.	Outputs instructions and information on how to use TreeFind"
+
     question = "Return to Main Menu?"
     output = %w(yes exit)
 
